7c4fcf4fffb5c0760d7bbd09a1ed49cfab96d1a5
adeepv <adeepv@gmail.com>
2015-10-20 14:26:41 +0300

    Add form logic

--- main/forms.py
+++ main/forms.py
@@ -4,10 +4,25 @@ from django import forms
 from django import forms
 
 class DateChartForm(forms.Form):
-    start_date = forms.DateField(label='Sdate')
-    start_time = forms.TimeField(widget=forms.TimeInput(format='%H:%M'))
-    end_date = forms.DateField(label='Edate')
-    end_time = forms.TimeField(widget=forms.TimeInput(format='%H:%M'))
-    type = forms.ChoiceField(choices=('5min','1hr','1day'))
+    sdt = forms.DateTimeField(label='From:')
+    ddt = forms.DateTimeField(label='To:')
+    type = forms.ChoiceField(choices=(('5min','5 min'),('1hr','1 hour'),('1day','1 day')))
+    vlan = forms.ChoiceField(label='VLAN', choices=(("126",'Dataline UA'),
+        ("2755",'Dataline World ACS'),
+        ("2768",'Dataline World KCT'),
+        ("777",'DTEL-IX'),
+        ("556",'Giganet-IX'),
+        ("557",'UA-IX'),
+        ("872",'Gigatrans-IX'),
+        ("929",'Yandex-IX'),
+        ("1013",'MailRu-IX'),
+        ("1730",'Fiord-IX'),
+        ("1740",'Fiord-World'),
+        ("2639",'TopNet-UA ACS'),
+        ("3317",'TopNet-UA KCT'),
+        ("3337",'Google-IX'),
+        ("3332",'Internal UANG-SMF'),
+        ("2715",'Multicast Kiev-SMF')
+                                                    ))
 
 
--- main/views.py
+++ main/views.py
@@ -12,32 +12,36 @@ from django.db.models import Sum
 from graphos.sources.model import ModelDataSource
 from graphos.renderers import gchart,highcharts,yui
 
+@render_to('result.html')
 def index(request):
     r = {} # result var
     sdate = datetime.date.today()
     ddate = datetime.date.today()
     stime = '00:00:00'
     dtime = '23:59:59'
-    vlan = '777'
-    type = '5min'
-    if request.method == 'POST':
-        form = DateChartForm(request.POST)
-        if form.is_valid():
-            ddate = form.end_date
-            sdate = form.start_date
-            stime = form.start_time
-            dtime = form.end_time
-            type = form.type
-
     sstime = datetime.datetime.strptime(stime,'%H:%M:%S')
     ddtime = datetime.datetime.strptime(dtime,'%H:%M:%S')
-
     fsdate = datetime.datetime.combine(sdate,datetime.time(sstime.hour,sstime.minute,sstime.second,0,None))
     fddate = datetime.datetime.combine(ddate,datetime.time(ddtime.hour,ddtime.minute,ddtime.second,0,None))
+    qsdate = fsdate.strftime('%Y-%m-%d %H:%M:%S')
+    qddate = fddate.strftime('%Y-%m-%d %H:%M:%S')
+    vlan = '777'
+    type = '5min'
+    if request.method and request.method == 'POST':
+        form = DateChartForm(request.POST)
+        if form.is_valid():
+            qsdate = form.sdt
+            qddate = form.ddt
+            type = form.type
+        else:
+            form = DateChartForm()
+    else:
+        form = DateChartForm()
+
     #result = AcctBgp5Mins.objects.only().extra(select={'peer_as_s':'CAST(peer_as_src AS CHAR(50))'}).filter(
     #stamp_inserted__gte=datetime.datetime.combine(sdate,datetime.time(sstime.hour,sstime.minute,sstime.second)),
     #stamp_inserted__lte=datetime.datetime.combine(ddate,datetime.time(ddtime.hour,ddtime.minute,ddtime.second)),vlan='777').annotate(Sum('bytes')).order_by('-bytes__sum')
-    result = AcctBgp5Mins.objects.raw('SELECT id, CAST(`peer_as_src` AS CHAR(50)) AS `peer_as_s`, CAST(SUM(`bytes`) AS INTEGER) as `bytes__sum`,id FROM `acct_bgp_5mins` WHERE vlan="%s" AND stamp_inserted >= "%s" AND stamp_inserted <= "%s" GROUP BY `peer_as_s` ORDER BY SUM(`bytes`) DESC'%(vlan,fsdate.strftime('%Y-%m-%d %H:%M:%S'),fddate.strftime('%Y-%m-%d %H:%M:%S')))
+    result = AcctBgp5Mins.objects.raw('SELECT id, CAST(`peer_as_src` AS CHAR(50)) AS `peer_as_s`, CAST(SUM(`bytes`) AS INTEGER) as `bytes__sum`,id FROM `acct_bgp_5mins` WHERE vlan="%s" AND stamp_inserted >= "%s" AND stamp_inserted <= "%s" GROUP BY `peer_as_s` ORDER BY SUM(`bytes`) DESC'%(vlan,qsdate,qddata))
     data_source = ModelDataSource(result,fields=['peer_as_s', 'bytes__sum'])
     tdata = []
     ii = 0
@@ -55,4 +59,5 @@ def index(request):
     r['charttitle'] = 'Traffic for vlan %s from %s to %s'%(vlan,fsdate.strftime('%Y-%m-%d %H:%M:%S'),fddate.strftime('%Y-%m-%d %H:%M:%S'))
     r['chartlegend'] = 'By ASN'
     r['chartdata'] = tdata
-    return render(request, 'result.html', r)
+    r['form']  = form
+    return r
